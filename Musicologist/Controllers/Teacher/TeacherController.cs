d954bd703d66cb6ef0bce96b5766091655538f5e
Mikael Lettestad <mikael.lettestad@gmail.com>
2023-02-02 02:17:19 +0100

    Add teacher page

---
+++ Musicologist/Controllers/Teacher/TeacherController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Musicologist.Controllers.Teacher
+{
+    public class TeacherController : Controller
+    {
+        public IActionResult AddCourse()
+        {
+            return View();
+        }
+    }
+}
---
+++ Musicologist/Repositories/CourseRepository.cs
@@ -0,0 +1,25 @@
+﻿using Musicologist.Data;
+using Musicologist.Models;
+using Musicologist.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Musicologist.Repositories
+{
+    public class CourseRepository : ICourseRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public CourseRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public void AddCourse(Course course)
+        {
+            _context.Courses.Add(course);
+        }
+    }
+}
